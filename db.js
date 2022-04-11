ec01c237027639e833a32841eea895ebcb22d9a9
Violetta Ryabtsev <wondervioletta@gmail.com>
2022-04-11 12:24:15 -0700

    created dovker-compose file and dockerfile for mysql database

--- db.js
+++ db.js
@@ -5,22 +5,18 @@ dotenv.config();
 
 const conn = mysql.createConnection({
   // host: `${process.env.DB_HOST}:3306`,
-  host: process.env.DB_HOST,
+  host: process.env.MYSQL_HOST || "localhost",
   // user: process.env.DB_USER,
   // password: process.env.DB_PASS,
   // database: process.env.DB_DATA,
-  database: process.env.MYSQL_DATABASE,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE || "mysql-blog",
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "root",
 });
 
 conn.connect((err) => {
   if (err) {
-    console.log(
-      "we did not connect to database:\n",
-
-      process.env.DB_USER
-    );
+    console.log("we did not connect to database:\n", process.env.MYSQL_USER);
     throw err;
   }
   console.log("connected to mySQL databases ");
---
+++ db/testdump.sql
@@ -0,0 +1,5 @@
+CREATE TABLE reviews(
+    name VARCHAR(60),
+    body VARCHAR(500),
+    id VARCHAR(30)
+);
